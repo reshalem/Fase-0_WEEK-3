d7635a9b134ed26862b92457db6312abd9f83847
reshalem <aliisetia@gmail.com>
2018-08-07 15:26:31 +0700

    Slight revision

--- looping_data_array.js
+++ looping_data_array.js
@@ -2,8 +2,8 @@ var input = [
 				["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
                 ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
                 ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
-                ["0004", "Bintang Senjaya", "Jayapura","6/4/1970", "Berkebun"]	
-			];		
+                ["0004", "Bintang Senjaya", "Jayapura","6/4/1970", "Berkebun"]
+			];
 
 function dataHandling(array) {
 	var bio = ['Nomor ID:  ', 'Nama Lengkap:  ', 'TTL:  ', 'Hobi:  ']; 	
