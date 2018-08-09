cf90c5d488ff1ae5d6c5234c9ff53fa7c514df6d
reshalem <aliisetia@gmail.com>
2018-08-09 21:23:46 +0700

    Revision

--- palindrome_angka.js
+++ palindrome_angka.js
@@ -1,9 +1,10 @@
 function palindrome(kata) {
 	var balik = '';
+	
 	for (var i = kata.length-1; i >= 0; i--) {
 		balik = balik + kata[i];
 	}
-	
+		
 	if (balik === kata) {
 		return true;
 	} else {
@@ -12,15 +13,11 @@ function palindrome(kata) {
 }
 
 function angkaPalindrome(num) {
-	var palNumber;
-	var numStr = num.toString();
-	
-	if (palindrome(numStr) === true || palindrome(numStr) === false) {
-		for (var i = num+1; i < 1500; i++) {
-			var x = i.toString();
-			if (palindrome(x) === true) {
-				return x;
-			}
+	for (var i = num+1; i < 1500; i++) {
+		var x = i.toString();
+
+		if (palindrome(x) === true) {
+			return Number(x);
 		}
 	}
 }
