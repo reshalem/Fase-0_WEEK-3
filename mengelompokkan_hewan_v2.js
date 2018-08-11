a72da353a7591962475aeb5c3da2df204ccf977b
reshalem <aliisetia@gmail.com>
2018-08-12 00:39:55 +0700

    Versi Final

---
+++ mengelompokkan_hewan_v2.js
@@ -0,0 +1,33 @@
+function groupAnimals(animals) {
+	var abjadDepan = '';
+	var temp = [];
+	var result = [];
+	var arrAbjad = [];
+	var temp = [];
+
+	for (var i = 0; i < animals.length; i++) {
+		if (arrAbjad.indexOf(animals[i][0]) === -1) {
+			arrAbjad.push(animals[i][0]);
+		}
+	}
+
+	for (var i = 0; i < arrAbjad.length; i++) {
+		for (var j = 0; j < animals.length; j++) {
+			abjadDepan = animals[j][0];
+
+			if (abjadDepan === arrAbjad[i]) {
+				temp.push(animals[j]);
+			}
+		}
+
+		result.push(temp);
+		temp = [];
+	}
+
+	result.sort();
+	return result;
+}
+
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil'])); // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
+// [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
